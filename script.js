94091810b493b0684d985d703e983c106d0f9e46
Reagan <reaganrandolph1015@gmail.com>
2022-01-24 16:40:55 -0600

    book class

---
+++ script.js
@@ -0,0 +1,12 @@
+class book {
+    constructor(title, author, pageNum, read) {
+        this.title = title;
+        this.author = author;
+        this.pageNum = pageNum;
+        this.read = read;
+    }
+}
+
+function addBook() {
+    prompt()
+}
