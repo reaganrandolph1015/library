4f9a4ec364058f7e7402b18503b498ea0359f765
Reagan <reaganrandolph1015@gmail.com>
2022-01-25 17:19:28 -0600

    Added and finished functions, working on user input and book display

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
 // array to hold user input for book information
 let myLibrary = [];
 
+const DEFAULT = [
+  { title: 'The Lord of the Rings', author: 'Tolkien', status: 'unread' },
+  { title: 'Alice in Wonderland', author: 'Lewis Caroll', status: 'unread' },
+  { title: 'Naruto', author: 'Masashi Kishimoto', status: 'unread' },
+];
+
 // vars for html element selection
 const $title = document.querySelector('#title');
 const $author = document.querySelector('#author');
 const $status = document.querySelector('#status');
+const tableBody = document.querySelector('#tableBody');
+const table = document.querySelector('table').addEventListener('click', (e) => {
+  const currentTarget = e.target.parentNode.parentNode.childNodes[1];
+  if (e.target.innerHTML == 'delete') {
+    if (confirm('are you sure you want to delete ${currentTarget.innerText}'))
+      deleteBook(findBook(myLibrary, currentTarget.innerText));
+  }
+  if (e.target.classList.contains('stat-button')) {
+    changeStatus(findBook(myLibrary, currentTarget.innerText));
+  }
+
+  updateStorage();
+  render();
+});
 const form = document.querySelector('form').addEventListener('submit', (e) => {
   // form submition event
+  e.preventDefault();
   addBook();
   render();
   clearForm();
@@ -43,7 +64,7 @@ function deleteBook(current) {
 }
 
 function findBook(libArray, title) {
-  if (libArray.length === 0 || libArray === numm) {
+  if (libArray.length === 0 || libArray === null) {
     return;
   }
   for ($book of libArray) {
@@ -61,3 +82,32 @@ function clearForm() {
 function updateStorage() {
   localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
 }
+
+function checkStorage() {
+  if (localStorage.getItem('myLibrary')) {
+    myLibrary = JSON.parse(localStorage.getItem('myLibrary'));
+  } else {
+    myLibrary = DEFAULT;
+  }
+}
+
+function render() {
+  checkStorage();
+
+  tableBody.innerHTML = '';
+
+  myLibrary.forEach(($book) => {
+    const htmlBook = `
+    <tr>
+    <td>${$book.title}</td>
+    <td>${$book.author}</td>
+    <td><button class="stat-button">${$book.status}</button></td>
+    <td><button class="delete">Delete</button></td>
+    </tr>
+    `;
+
+    tableBody.insertAdjacentHTML('afterbegin', htmlBook);
+  });
+}
+
+render();
