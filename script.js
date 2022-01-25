658b1a978f19949ca34d8ee23d141b7cb44747b2
Reagan <reaganrandolph1015@gmail.com>
2022-01-25 08:47:32 -0600

    Minor

--- script.js
+++ script.js
@@ -1,72 +1,58 @@
 let myLibrary = [];
 
 class book {
-    letructor(title, author, pages, read) {
-        this.title = title;
-        this.author = author;
-        this.pages = pages;
-        this.read = read;
-    }
-}
-
-function displayBooks() {
-    for (i = 0; i <= myLibrary.length; i++) {
-        
-    }
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
 }
 
 function createBook(item) {
-    let library = document.querySelector('#library-container');
-    let bookDiv = document.createElement('div');
-    let titleDiv = document.createElement('div');
-    let authDiv = document.createElement('div');
-    let pageDiv = document.createElement('div');
-    let removeBtn = document.createElement('button');
-    let readBtn = document.createElement('button');
+  let library = document.querySelector('#library-container');
+  let bookDiv = document.createElement('div');
+  let titleDiv = document.createElement('div');
+  let authDiv = document.createElement('div');
+  let pageDiv = document.createElement('div');
+  let removeBtn = document.createElement('button');
+  let readBtn = document.createElement('button');
 
-    bookDiv.classList.add('book');
-    bookDiv.setAttribute('id', myLibrary.indexOf(item));
+  bookDiv.classList.add('book');
+  bookDiv.setAttribute('id', myLibrary.indexOf(item));
 
-    titleDiv.textContent = item.title;
-    titleDiv.classList.add('title');
-    bookDiv.appendChild(titleDiv);
+  titleDiv.textContent = item.title;
+  titleDiv.classList.add('title');
+  bookDiv.appendChild(titleDiv);
 
-    authDiv.textContent = item.author;
-    authDiv.classList.add('author');
-    bookDiv.appendChild(titleDiv);
-    
-    pageDiv.textContent = item.pages;
-    pageDiv.classList.add('pages');
+  authDiv.textContent = item.author;
+  authDiv.classList.add('author');
+  bookDiv.appendChild(titleDiv);
 
-    readBtn.classList.add('read-btn');
-    bookDiv.appendChild(readBtn);
-    if (item.read === false ) {
-        readBtn.textContent = "Unread";
-        readBtn.style.backgroundColor = '#e04f63';
-    } else {
-        readBtn.textContent = "Read";
-        readBtn.style.backgroundColor = '#63da63';
-    }
+  pageDiv.textContent = item.pages;
+  pageDiv.classList.add('pages');
 
-    removeBtn.textContent = "Remove";
-    removeBtn.setAttribute('id', 'remove-btn');
-    bookDiv.appendChild(removeBtn);
+  readBtn.classList.add('read-btn');
+  bookDiv.appendChild(readBtn);
+  if (item.read === false) {
+    readBtn.textContent = 'Unread';
+    readBtn.style.backgroundColor = '#e04f63';
+  } else {
+    readBtn.textContent = 'Read';
+    readBtn.style.backgroundColor = '#63da63';
+  }
 
-    library.appendChild(bookDiv);
+  removeBtn.textContent = 'Remove';
+  removeBtn.setAttribute('id', 'remove-btn');
+  bookDiv.appendChild(removeBtn);
 
-    removeBtn.addEventListener('click', () => {
-        myLibrary.spliace(myLibrary.indexOf(item), 1);
-        ()
-        ()
-    });
+  library.appendChild(bookDiv);
 
-    readBtn.addEventListener('click', () => {
-        item.read = !item.read;
-        ()
-        ()
-    });
-}
+  removeBtn.addEventListener('click', () => {
+    myLibrary.spliace(myLibrary.indexOf(item), 1);
+  });
 
-function addBook() {
-    prompt()
+  readBtn.addEventListener('click', () => {
+    item.read = !item.read;
+  });
 }
